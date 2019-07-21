ed3c55d80724a7bc88c50ab09e316bf25b657b53
Kurenai <kurenai233@yahoo.com>
2019-07-22 00:58:46 +0800

    feat(): 添加MybatisPlus依赖，将符号字符串改为使用常量类表示，增加readme

--- src/main/java/io/github/natsusai/cache/DefaultKeyGenerator.java
+++ src/main/java/io/github/natsusai/cache/DefaultKeyGenerator.java
@@ -1,5 +1,6 @@
 package io.github.natsusai.cache;
 
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,6 +8,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 
 /**
+ * 默认缓存键值生成器
+ *
  * @author Kurenai
  * @since 2019-07-21 20:49
  */
@@ -20,7 +23,7 @@ public class DefaultKeyGenerator implements KeyGenerator {
     }
 
     public String generateCacheName(String prefix, String appName, String clazzName, String methodName) {
-        return String.join(":", Arrays.asList(prefix, appName.toUpperCase(), clazzName)) + "#" + methodName;
+        return String.join(StringPool.COLON, Arrays.asList(prefix, appName.toUpperCase(), clazzName)) + StringPool.HASH + methodName;
     }
 
     public String generateCacheKey(Object[] args) {
